2d4a81a5993f0d8db483d2de984605dfcae09223
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2025-09-27 16:58:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function setup(){
   imageMode(CENTER);
   // begin button
   beginx = windowWidth*0.3;
-  beginy = windowHeight*0.7;
+  beginy = windowHeight*0.6;
   beginw = 142;
   beginh = 47;
   // next button
